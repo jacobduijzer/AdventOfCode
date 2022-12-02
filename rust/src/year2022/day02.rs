3b4a00aba453f2abe6b848435e2f7ce44b2c623b
Jacob Duijzer <jacob@duijzer.com>
2022-12-02 09:33:56 +0100

    feat: day 2 in rust

--- rust/src/year2022/day02.rs
+++ rust/src/year2022/day02.rs
@@ -1,11 +1,45 @@
 use crate::common;
 
 pub fn solve_part1(input: &str) -> u32 {
-    unimplemented!()
+    input
+        .lines()
+        .map(|l| l.split_once(" ").unwrap())
+        .fold(0u32, |score, (opp, me)| {
+            match (opp, me) {
+                ("A", "Z") => score + 3,
+                ("B", "X") => score + 1,
+                ("C", "Y") => score + 2,
+                ("C", "X") => score + 6 + 1,
+                ("A", "Y") => score + 6 + 2,
+                ("B", "Z") => score + 6 + 3,
+                (_, "X") => score + 3 + 1,
+                (_, "Y") => score + 3 + 2,
+                (_, "Z") => score + 3 + 3,
+                _ => unreachable!(),
+            }
+        })
+        .into()
 }
 
 pub fn solve_part2(input: &str) -> u32 {
-    unimplemented!()
+    input
+        .lines()
+        .map(|l| l.split_once(" ").unwrap())
+        .fold(0u32, |score, (opp, me)| {
+            match (opp, me) {
+                ("A", "X") => score + 3,
+                ("B", "X") => score + 1,
+                ("C", "X") => score + 2,
+                ("A", "Y") => score + 4,
+                ("B", "Y") => score + 5,
+                ("C", "Y") => score + 6,
+                ("A", "Z") => score + 8,
+                ("B", "Z") => score + 9,
+                ("C", "Z") => score + 7,
+                _ => unreachable!(),
+            }
+        })
+        .into()
 }
 
 #[cfg(test)]
@@ -14,13 +48,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let input = common::input::read_file(2022, 1, "testinput");
-        assert_eq!(solve_part1(&input), 24000);
+        let input = common::input::read_file(2022, 2, "testinput");
+        assert_eq!(solve_part1(&input), 15);
     }
 
     #[test]
     fn test_part_two() {
-        let input = common::input::read_file(2022, 1, "testinput");
-        assert_eq!(solve_part2(&input), 45000);
+        let input = common::input::read_file(2022, 2, "testinput");
+        assert_eq!(solve_part2(&input), 12);
     }
 }
