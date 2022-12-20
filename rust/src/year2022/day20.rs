e1159b4ea4f0803562abc340372d83283726b068
Jacob Duijzer <jacob@duijzer.com>
2022-12-20 10:40:07 +0100

    feat: add timing for day 20

--- rust/src/year2022/day20.rs
+++ rust/src/year2022/day20.rs
@@ -1,4 +1,12 @@
-fn mix_and_decrypt(coordinates: Vec<i64>, mix: usize) -> i64 {
+use std::borrow::{Borrow, BorrowMut};
+
+fn decrypt(input: &str, decrypt_key: i64) -> Vec<i64> {
+    input.lines()
+        .map(|line| line.parse::<i64>().unwrap() * decrypt_key)
+        .collect::<Vec<i64>>()
+}
+
+fn mix(coordinates: Vec<i64>, mix: usize) -> Vec<usize> {
     let mut to_sort = (0..coordinates.len()).collect::<Vec<_>>();
     for _ in 0..mix {
         let mut i = 0;
@@ -19,34 +27,40 @@ fn mix_and_decrypt(coordinates: Vec<i64>, mix: usize) -> i64 {
         }
     }
 
-    let ordered_zero_position = to_sort[coordinates.iter().position(|&x| x == 0).unwrap()];
+    to_sort
+}
+
+fn calculate_result(coordinates: Vec<i64>, mixed: Vec<usize>) -> i64 {
+    let ordered_zero_position = mixed[coordinates.iter().position(|&x| x == 0).unwrap()];
     let first_index = (ordered_zero_position + 1000).rem_euclid(coordinates.len());
     let second_index = (ordered_zero_position + 2000).rem_euclid(coordinates.len());
     let third_index = (ordered_zero_position + 3000).rem_euclid(coordinates.len());
 
-    let first = coordinates[to_sort.iter().position(|&x| x == first_index).unwrap()];
-    let second = coordinates[to_sort.iter().position(|&x| x == second_index).unwrap()];
-    let third = coordinates[to_sort.iter().position(|&x| x == third_index).unwrap()];
+    let first = coordinates[mixed.iter().position(|&x| x == first_index).unwrap()];
+    let second = coordinates[mixed.iter().position(|&x| x == second_index).unwrap()];
+    let third = coordinates[mixed.iter().position(|&x| x == third_index).unwrap()];
 
     (first + second + third)
 }
 
 pub fn solve_part1(input: &str) -> i64 {
-
-    let coordinates = input.lines()
-        .map(|line| line.parse::<i64>().unwrap())
-        .collect::<Vec<i64>>();
-
-    mix_and_decrypt(coordinates, 1)
+    let (took, mut coordinates) = took::took(|| decrypt(input, 1));
+    println!("Time spent decrypting: {}", took);
+    let (took, mixed) = took::took(|| mix(coordinates.clone(), 1));
+    println!("Time spent mixing: {}", took);
+    let (took, result) = took::took(|| calculate_result(coordinates.clone(), mixed));
+    println!("Time spent calculating result: {}", took);
+    result
 }
 
 pub fn solve_part2(input: &str) -> i64 {
-
-    let coordinates = input.lines()
-        .map(|line| line.parse::<i64>().unwrap() * 811589153)
-        .collect::<Vec<i64>>();
-
-    mix_and_decrypt(coordinates, 10)
+    let (took, coordinates) = took::took(|| decrypt(input,811589153 ));
+    println!("Time spent decrypting: {}", took);
+    let (took, mixed) = took::took(|| mix(coordinates.clone(), 10));
+    println!("Time spent mixing: {}", took);
+    let (took, result) = took::took(|| calculate_result(coordinates.clone(), mixed));
+    println!("Time spent calculating result: {}", took);
+    result
 }
 
 #[cfg(test)]
