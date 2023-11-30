24133fa649f570743cb3f301505da05f6984e9f4
Jacob Duijzer <jacob@duijzer.com>
2023-11-30 09:44:54 +0100

    parameters for advent of code script

--- polybar/adventofcode_polybar.sh
+++
@@ -1,3 +0,0 @@
-cookie=$(head -n 1 $HOME/.aoc_cookie.txt)
-curl -s https://adventofcode.com/2022/leaderboard/private/view/1538851.json -X GET -H "Cookie: session=$cookie" > /tmp/aoc.json
-jq '[.members[] | { position: .key, score: .local_score, name: .name }]|sort_by(.score)|reverse|sort_by(.last_star_ts)|to_entries|.[] | select(.value.name=="Jacob Duijzer")|"AoC: position: \(.key+1), score: \(.value.score)"' /tmp/aoc.json | sed 's/\"//g'
