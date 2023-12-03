88d771bbd70beb8f75dbbeb57ea3f643935049fd
Jacob Duijzer <jacob@duijzer.com>
2023-12-03 19:33:41 +0100

    2023, day 2 csharp visualization

---
+++ csharp/2023/src/AdventOfCode2023.UI/Visualisations/Day01.cs
@@ -0,0 +1,65 @@
+using AdventOfCode2023.Core;
+using Spectre.Console;
+
+namespace AdventOfCode2023.UI.Visualisations;
+
+public static class Day01
+{
+    public static async Task Run()
+    {
+        // SETTINGS
+        const int NumberOfRows = 10;
+        const int CharacterDelay = 50;
+        const int LineDelay = 50;
+
+        var day01 = new Part1();
+
+        var directory = AppContext.BaseDirectory;
+        var fileName = "day01.txt";
+
+        var input = File.ReadAllLines(Path.Combine(directory, "input", fileName));
+
+        var table = new Table().Expand().BorderColor(Color.Grey);
+        table.AddColumn("[yellow]Calibration Line[/]");
+        table.AddColumn(new TableColumn("[yellow]Calibration Value[/]").Centered());
+        table.AddColumn(new TableColumn("[yellow]Total Calibration Sum[/]").Centered());
+
+        AnsiConsole.MarkupLine("Press [yellow]CTRL+C[/] to exit");
+
+        await AnsiConsole.Live(table)
+            .AutoClear(false)
+            .Overflow(VerticalOverflow.Ellipsis)
+            .Cropping(VerticalOverflowCropping.Bottom)
+            .StartAsync(async ctx =>
+            {
+                var currentLine = 0;
+                foreach (var line in input)
+                {
+                    if (table.Rows.Count > NumberOfRows)
+                    {
+                        table.Rows.RemoveAt(0);
+                        currentLine--;
+                    }
+
+                    day01.SetLine(line);
+                    table.AddRow(line, "0", "0");
+
+                    for (var i = 0; i < line.Length; i++)
+                    {
+                        day01.RecoverNext();
+                        table.Rows.Update(currentLine, 0, new Markup(day01.GetNewLine()));
+                        ctx.Refresh();
+                        await Task.Delay(CharacterDelay);
+                    }
+
+                    table.Rows.Update(currentLine, 1, new Markup(day01.LineResult().ToString()));
+                    table.Rows.Update(currentLine, 2, new Markup(day01.TotalResult().ToString()));
+
+                    currentLine++;
+
+                    ctx.Refresh();
+                    await Task.Delay(LineDelay);
+                }
+            });
+    }
+}
---
+++ csharp/2023/src/AdventOfCode2023.UI/Visualisations/Day02.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+using AdventOfCode2023.Core.day02;
+using Spectre.Console;
+
+namespace AdventOfCode2023.UI.Visualisations;
+
+public static class Day02
+{
+    public static async Task Run()
+    {
+        var directory = AppContext.BaseDirectory;
+        var fileName = "day02.txt";
+
+        var input = File.ReadAllLines(Path.Combine(directory, "input", fileName));
+        GameHistory gameHistory = new GameHistory(input);
+
+        await AnsiConsole.Progress()
+            .StartAsync(async ctx =>
+            {
+                // Define tasks
+                var task1 = ctx.AddTask("[red]Red[/]");
+                var task2 = ctx.AddTask("[green]Green[/]");
+                var task3 = ctx.AddTask("[blue]Blue[/]");
+                var task4 = ctx.AddTask("Total Games");
+                // var task5 = ctx.AddTask("Total Draws");
+
+                for (var i = 0; i <= gameHistory.Games.Count; i++)
+                {
+                    foreach (var draw in gameHistory.Games[i].Draws)
+                    {
+                        // Simulate some work
+                        await Task.Delay(250);
+
+                        var red = draw.Cubes.Any(x => x.Color.Equals("red"))
+                            ? draw.Cubes.FirstOrDefault(x => x.Color.Equals("red")).Amount
+                            : 0;
+                        var green = draw.Cubes.Any(x => x.Color.Equals("green"))
+                            ? draw.Cubes.FirstOrDefault(x => x.Color.Equals("green")).Amount
+                            : 0;
+                        var blue = draw.Cubes.Any(x => x.Color.Equals("blue"))
+                            ? draw.Cubes.FirstOrDefault(x => x.Color.Equals("blue")).Amount
+                            : 0;
+
+                        // Increment
+                        task1.Value((int)((red / 12.0) * 100));
+                        task2.Value((int)((green / 13.0) * 100));
+                        task3.Value((blue / 14.0) * 100);
+                        task4.Value = (i / (double)gameHistory.Games.Count) * 100;
+                        //task4.Value = (i / (double)gameHistory.Games.Count) * 100;
+                    }
+                }
+            });
+    }
+}
