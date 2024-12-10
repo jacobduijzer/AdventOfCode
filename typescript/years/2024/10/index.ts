7cd6b32907187e14696a72eda75d9c6112d73638
Jacob Duijzer <jacob@duijzer.com>
2024-12-10 08:55:02 +0100

    2024, day 10, pt 1, typescript

--- typescript/years/2024/10/index.ts
+++ typescript/years/2024/10/index.ts
@@ -5,6 +5,7 @@ import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
 import { normalizeTestCases } from "../../../util/test";
+import { Grid } from "../../../util/grid";
 
 const YEAR = 2024;
 const DAY = 10;
@@ -13,8 +14,66 @@ const DAY = 10;
 // data path    : C:\Users\jacob\Code\github\AdventOfCode\typescript\years\2024\10\data.txt
 // problem url  : https://adventofcode.com/2024/day/10
 
+const directions = [
+	[0, 1],  // right
+	[1, 0],  // down
+	[0, -1], // left
+	[-1, 0], // up
+];
+
+function dfs(
+	grid: Grid,
+	x: number,
+	y: number,
+	currentValue: number,
+	visitedNines: Set<string>
+) {
+	const rows = grid.rowCount;
+	const cols = grid.colCount;
+
+	if (x < 0 || y < 0 || x >= rows || y >= cols)
+		return;
+
+	const currentCellValue = Number(grid.getValue([x, y]));
+
+	if (currentCellValue !== currentValue)
+		return;
+
+	const key = `${x},${y}`;
+	if (currentCellValue === 9) {
+		if (!visitedNines.has(key)) {
+			visitedNines.add(key); // Mark this 9 as visited globally
+		} else {
+			// This 9 has already been counted
+			return
+		}
+	}
+
+	for (const [dx, dy] of directions) {
+		const newX = x + dx;
+		const newY = y + dy;
+		dfs(grid, newX, newY, currentValue + 1, visitedNines);
+	}
+}
+
 async function p2024day10_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const grid = new Grid({ serialized: input });
+
+	const rows = grid.rowCount;
+	const cols = grid.colCount;
+	let pathCount = 0;
+
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			if (Number(grid.getValue([i, j])) === 0) {
+				const visitedNines = new Set<string>();
+				dfs(grid, i, j, 0, visitedNines);
+				pathCount += visitedNines.size;
+			}
+		}
+	}
+
+	return pathCount;
 }
 
 async function p2024day10_part2(input: string, ...params: any[]) {
@@ -22,7 +81,26 @@ async function p2024day10_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `0123
+1234
+8765
+9876`,
+		extraArgs: [],
+		expected: `1`
+	},
+		{
+			input: `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`,
+			extraArgs: [],
+			expected: `36`
+		}];
 	const part2tests: TestCase[] = [];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
