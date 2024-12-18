310ad6d29c56e47d0e69bf0a8a8632d566990af7
Jacob Duijzer <jacob@duijzer.com>
2024-12-18 13:26:55 +0100

    2024, day 17, pt2, typescript

--- typescript/years/2024/17/index.ts
+++ typescript/years/2024/17/index.ts
@@ -4,7 +4,7 @@ import { normalizeTestCases } from "../../../util/test";
 import chalk from "chalk";
 import { log, logSolution } from "../../../util/log";
 import { performance } from "perf_hooks";
-import { start } from "node:repl";
+
 
 const YEAR = 2024;
 const DAY = 17;
@@ -14,108 +14,123 @@ const DAY = 17;
 // problem url  : https://adventofcode.com/2024/day/17
 
 type FullProgram = {
-	A: number;
-	B: number;
-	C: number;
+	A: bigint;
+	B: bigint;
+	C: bigint;
 
-	Program: number[];
+	Program: bigint[];
 }
 function parseInput(input: string): FullProgram {
 	const registerMatches = input.match(/Register [A-Z]: (\d+)/g);
-	const registers = registerMatches ? registerMatches.map(match => parseInt(match.split(': ')[1], 10)) : [0, 0, 0];
+	const registers = registerMatches ? registerMatches.map(match => BigInt(match.split(': ')[1]), 10n) : [0n, 0n, 0n];
 
 	const programMatch = input.match(/Program: ([\d,]+)/);
-	const program = programMatch ? programMatch[1].split(',').map(Number) : [];
+	const program = programMatch ? programMatch[1].split(',').map(BigInt) : [];
 
 	return { A: registers[0], B: registers[1], C: registers[2], Program: program };
 }
 
-function getComboValue(operand: number, program: FullProgram): number {
+function getComboValue(operand: bigint, program: FullProgram): bigint{
 	switch (operand) {
-		case 0: case 1: case 2: case 3:
+		case 0n: case 1n: case 2n: case 3n:
 			return operand;
-		case 4: return program.A;
-		case 5: return program.B;
-		case 6: return program.C;
+		case 4n: return program.A;
+		case 5n: return program.B;
+		case 6n: return program.C;
 		default:
 			throw new Error(`Invalid combo operand: ${operand}`);
 	}
 }
 
-function adv(operand: number, program: FullProgram): number {
-	const denominator = Math.pow(2, getComboValue(operand, program));
-	return Math.floor(program.A / denominator);
+function adv(operand: bigint, program: FullProgram): bigint {
+	const denominator = 2n ** getComboValue(operand, program);
+	return program.A / denominator;
 }
 
-function bxl(operand: number, program: FullProgram): number {
-	return program.B  ^= operand;
+function bxl(operand: bigint, program: FullProgram): bigint {
+	return program.B ^= operand;
 }
 
-function bst(operand: number, program: FullProgram): number{
-	return getComboValue(operand, program) % 8;
+function bst(operand: bigint, program: FullProgram): bigint {
+	return getComboValue(operand, program) % 8n;
 }
 
-function jnz(operand: number, program: FullProgram, instructionPointer: number): number {
-	if (program.A !== 0) {
+function jnz(operand: bigint, program: FullProgram, instructionPointer: bigint): bigint {
+	if (program.A !== 0n) {
 		return operand;
 	}
-	return instructionPointer + 2; // Move to the next instruction as usual
+	return instructionPointer + 2n; // Move to the next instruction as usual
 }
 
-function out(operand: number, program: FullProgram): number {
-	return getComboValue(operand, program) % 8;
+function out(operand: bigint, program: FullProgram): bigint {
+	return getComboValue(operand, program) % 8n;
 }
 
-function bdv(operand: number, program: FullProgram): number {
-	const denominator = Math.pow(2, getComboValue(operand, program));
-	return Math.floor(program.A / denominator);
+function bdv(operand: bigint, program: FullProgram): bigint{
+	const denominator = 2n ** getComboValue(operand, program);
+	return program.A / denominator;
 }
 
-function cdv(operand: number, program: FullProgram): number {
-	const denominator = Math.pow(2, getComboValue(operand, program));
-	return Math.floor(program.A / denominator);
+function cdv(operand: bigint, program: FullProgram): bigint {
+	const denominator = 2n ** getComboValue(operand, program);
+	return program.A / denominator;
 }
 
-function runProgram(program: FullProgram) : number[] {
-	let instructionPointer = 0;
-	let output: number[] = [];
+function runProgram(program: FullProgram) : bigint[] {
+	let instructionPointer = 0n;
+	let output: bigint[] = [];
 
 	while (instructionPointer < program.Program.length) {
-		const opcode = program.Program[instructionPointer];
-		const operand = program.Program[instructionPointer + 1];
+		const opcode = program.Program[Number(instructionPointer)];
+		const operand = program.Program[Number(instructionPointer) + 1];
 
 		switch (opcode) {
-			case 0:
+			case 0n:
 				program.A = adv(operand, program);
 				break;
-			case 1:
+			case 1n:
 				program.B = bxl(operand, program);
 				break;
-			case 2:
+			case 2n:
 				program.B = bst(operand, program);
 				break;
-			case 3:
-				instructionPointer= jnz(operand, program, instructionPointer);
+			case 3n:
+				instructionPointer = jnz(operand, program, instructionPointer);
 				continue;
-			case 4:
+			case 4n:
 				program.B ^= program.C;
 				break;
-			case 5:
+			case 5n:
 				output.push(out(operand, program));
 				break;
-			case 6:
+			case 6n:
 				program.B = bdv(operand, program);
 				break;
-			case 7:
+			case 7n:
 				program.C = cdv(operand, program);
 				break;
 			default:
 				throw new Error(`Unknown opcode: ${opcode}`);
 		}
 
-		instructionPointer += 2; // Move to the next instruction
+		instructionPointer += 2n; // Move to the next instruction
 	}
 	return output;
+}
+
+function searchLowestA(value: bigint, current: number, fullProgram: FullProgram): bigint {
+	if (current < 0) return value;
+
+	for (let i = value << 3n; i < (value << 3n) + 8n; i++) {
+		fullProgram.A = i;
+		const output = runProgram(fullProgram);
+		if (output[0] === fullProgram.Program[current]) {
+			const finalVal = searchLowestA(i, current - 1, fullProgram);
+			if (finalVal !== -1n) return finalVal;
+		}
+	}
+
+	return -1n;
 }
 
 async function p2024day17_part1(input: string, ...params: any[]) {
@@ -123,61 +138,9 @@ async function p2024day17_part1(input: string, ...params: any[]) {
 	return runProgram(fullProgram).join(',');
 }
 
-const outFromA = (a: number) => {
-	const interim = (a % 8) ^ 3;
-	const c = Math.floor(a / Math.pow(2, interim));
-	const postC = c ^ 6;
-	return (postC ^ (a % 8)) % 8;
-	// return (a >> 3) % 8; // for example
-}
-
 async function p2024day17_part2(input: string, ...params: any[]) {
 	const fullProgram = parseInput(input);
-	// let candidates = [0];
-	// for (let i = fullProgram.Program.length - 1; i >= 0; i--) {
-	// 	const newCandidates = [];
-	// 	// console.log(`Instruction: ${fullProgram.Program[i]}, candidates: ${candidates}`);
-	// 	for (const c of candidates) {
-	// 		for (let j = 0; j < 8; j++) {
-	// 			const num = (c << 3) + j;
-	// 			const out1 = outFromA(num);
-	// 			console.log(`Num: ${num}, Out: ${out1}, Instruction: ${fullProgram.Program[i]}`);
-	//
-	// 			fullProgram.A = num;
-	// 			const out = runProgram(fullProgram);
-	// 			if(out.join(',') === fullProgram.Program.join(',')) {
-	// 				newCandidates.push(num);
-	// 			}
-	// 		}
-	// 	}
-	// 	candidates = newCandidates;
-	// }
-	// let candidates = [0];
-	// for (let i = fullProgram.Program.length - 1; i >= 0; i--) {
-	// 	const newCandidates = [];
-	// 	console.log(`Instruction: ${fullProgram.Program[i]}, candidates: ${candidates}`);
-	// 	for (const c of candidates) {
-	// 		for (let j = 0; j < 8; j++) {
-	// 			const num = (c << 3) + j;
-	// 			const out = outFromA(num);
-	// 			console.log(`Num: ${num}, Out: ${out}, Instruction: ${fullProgram.Program[i]}`);
-	// 			fullProgram.A = num;
-	// 			const output = runProgram(fullProgram);
-	// 			if(output.join(',') === fullProgram.Program.join(',')) {
-	// 				console.log("bingo");
-	// 			}
-	// 			if (out === fullProgram.Program[i]) {
-	// 				newCandidates.push(num);
-	// 			}
-	// 		}
-	// 	}
-	// 	candidates = newCandidates;
-	// }
-	//
-	// // console.log(candidates);
-	// return Math.min(...candidates);
-	//return findInitialA(0, fullProgram, fullProgram.Program.length - 1);
-	return "Not implemented";
+	return searchLowestA(0n, fullProgram.Program.length - 1, fullProgram);
 }
 
 async function run() {
@@ -233,7 +196,7 @@ Program: 0,3,5,4,3,0`,
 	const part1After = performance.now();
 
 	const part2Before = performance.now()
-	const part2Solution = String(await p2024day17_part2(input, 5000000000));
+	const part2Solution = String(await p2024day17_part2(input));
 	const part2After = performance.now();
 
 	logSolution(17, 2024, part1Solution, part2Solution);
